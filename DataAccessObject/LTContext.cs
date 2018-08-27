ece6fda02cc224c60420aba6ac48317a23f3231c
Leo <leo.formaggi@gmail.com>
2018-08-27 17:08:52 -0300

    Criado Context do DAO

---
+++ DataAccessObject/LTContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+
+namespace DataAccessObject
+{
+    public class LTContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Diretorio> Diretorios { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}

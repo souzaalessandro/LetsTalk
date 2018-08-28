6608fbe8b768541087c61c8c660c939fd1cbc27a
Leo <leo.formaggi@gmail.com>
2018-08-28 20:08:28 -0300

    Projeto DAO

---
+++ DataAccessObject/LTContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+
+namespace DataAccessObject
+{
+    public class LTContext : DbContext
+    {
+        public LTContext() : base("LTContext")
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Diretorio> Diretorios { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}

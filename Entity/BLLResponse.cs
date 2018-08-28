42c189e12f4ae3374aa0a9aca6b0d2993340c124
Leo <leo.formaggi@gmail.com>
2018-08-28 20:07:21 -0300

    Projeto Entity com classes model para o entity, enums, interfaces, utilidades e etc

---
+++ Entity/BLLResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class BLLResponse<T> where T: class
+    {
+        public bool Sucesso { get; set; }
+        public List<T> DataList { get; set; }
+        public T Data { get; set; }
+        public string Mensagem { get; set; }
+    }
+}
---
+++ Entity/Diretorio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Diretorio
+    {
+        public int ID { get; set; }
+        public string FullPath { get; set; }
+        public Usuario Usuario { get; set; }
+    }
+}

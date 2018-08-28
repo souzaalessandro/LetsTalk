af3bbc815b32e0050254530344c861c4fd0c9f32
Leo <leo.formaggi@gmail.com>
2018-08-28 14:50:42 -0300

    Classe cloner, UsuarioBLL com nova implementação

--- BusinessLogicalLayer/UsuarioBLL.cs
+++ BusinessLogicalLayer/UsuarioBLL.cs
@@ -11,34 +11,72 @@ namespace BusinessLogicalLayer
 {
     public class UsuarioBLL : IInsertable<Usuario>, IUpdatable<Usuario>, ISearchable<Usuario>, IDeletable<Usuario>
     {
-        public BLLResponse<Usuario> Delete(Usuario item)
-        {
-            throw new NotImplementedException();
-        }
-
         public BLLResponse<Usuario> Insert(Usuario item)
         {
+            //Fazer validações
+
             BLLResponse<Usuario> response = new BLLResponse<Usuario>();
             using (LTContext ctx = new LTContext())
             {
                 ctx.Usuarios.Add(item);
                 ctx.SaveChanges();
             }
-        }
-
-        public BLLResponse<Usuario> LerPorId(Usuario item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public BLLResponse<Usuario> LerTodos()
-        {
-            throw new NotImplementedException();
+            response.Sucesso = true;
+            response.Mensagem = "Cadastrado com sucesso.";
+            return response;
         }
 
         public BLLResponse<Usuario> Update(Usuario item)
         {
-            throw new NotImplementedException();
+            //Fazer validações
+
+            BLLResponse<Usuario> response = new BLLResponse<Usuario>();
+            Usuario user = new Usuario();
+            using (LTContext ctx = new LTContext())
+            {
+                user = ctx.Usuarios.FirstOrDefault(u => u.ID == item.ID);
+                response.Sucesso = user != null;
+                user.ClonarDe(item);
+                ctx.SaveChanges();
+            }
+            return response;
+        }
+
+        public BLLResponse<Usuario> Delete(Usuario item)
+        {
+            BLLResponse<Usuario> response = new BLLResponse<Usuario>();
+            using (LTContext ctx = new LTContext())
+            {
+                Usuario user = ctx.Usuarios.FirstOrDefault(u => u.ID == item.ID);
+                ctx.Usuarios.Remove(user);
+                ctx.SaveChanges();
+            }
+            response.Sucesso = true;
+            response.Mensagem = "Deletado com sucesso.";
+            return response;
+        }
+
+        public BLLResponse<Usuario> LerPorId(Usuario item)
+        {
+            BLLResponse<Usuario> response = new BLLResponse<Usuario>();
+            Usuario user = new Usuario();
+            using (LTContext ctx = new LTContext())
+            {
+                user = ctx.Usuarios.FirstOrDefault(u => u.ID == item.ID);
+            }
+            response.Sucesso = user != null;
+            response.Data = user;
+            return response;
+        }
+
+        public BLLResponse<Usuario> LerTodos()
+        {
+            BLLResponse<Usuario> response = new BLLResponse<Usuario>();
+            using (LTContext ctx = new LTContext())
+            {
+                response.DataList = ctx.Usuarios.ToList();
+            }
+            return response;
         }
     }
 }

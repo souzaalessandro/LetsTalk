c4e6f54101fb985e4b2ace2caad865adb2ae62b5
samuel borges <samulborges.exe@hotmail.com>
2018-08-28 15:31:10 -0300

    Adicionada a classe "TagBLL".

---
+++ BusinessLogicalLayer/TagBLL.cs
@@ -0,0 +1,38 @@
+﻿using Entity;
+using Entity.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicalLayer
+{
+    public class TagBLL : IInsertable<Tag>, IUpdatable<Tag>, ISearchable<Tag>, IDeletable<Tag>
+    {
+        public BLLResponse<Tag> Delete(Tag item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BLLResponse<Tag> Insert(Tag item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BLLResponse<Tag> LerPorId(Tag item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BLLResponse<Tag> LerTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public BLLResponse<Tag> Update(Tag item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

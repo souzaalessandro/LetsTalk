5583227bad759ecc767705444126d41ab555d500
samuel borges <samulborges.exe@hotmail.com>
2018-08-28 16:56:44 -0300

    CRUD padrão feito na "TagBLL", não consegui ter uma noção muito grande de regra de negócio, veremos com o tempo.

--- BusinessLogicalLayer/TagBLL.cs
+++ BusinessLogicalLayer/TagBLL.cs
@@ -13,7 +13,7 @@ namespace BusinessLogicalLayer
     {
         public BLLResponse<Tag> Delete(Tag item)
         {
-           
+
             BLLResponse<Tag> response = new BLLResponse<Tag>();
             using (LTContext ctx = new LTContext())
             {
@@ -53,12 +53,27 @@ namespace BusinessLogicalLayer
 
         public BLLResponse<Tag> LerTodos()
         {
-            throw new NotImplementedException();
+            BLLResponse<Tag> response = new BLLResponse<Tag>();
+            using (LTContext ctx = new LTContext())
+            {
+                response.DataList = ctx.Tags.ToList();
+            }
+            return response;
         }
 
         public BLLResponse<Tag> Update(Tag item)
         {
-            throw new NotImplementedException();
+            BLLResponse<Tag> response = new BLLResponse<Tag>();
+            Tag tag = new Tag();
+            using (LTContext ctx = new LTContext())
+            {
+                tag = ctx.Tags.FirstOrDefault(u => u.ID == item.ID);
+                response.Sucesso = tag != null;
+                tag.ClonarDe(item);
+                ctx.SaveChanges();
+            }
+            return response;
         }
     }
 }
+

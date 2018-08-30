160d4caa282dedd4efa2d9962b2882e9401e747e
Leo <leo.formaggi@gmail.com>
2018-08-30 16:12:38 -0300

    Extension para facilitar a vida na checagem de string nula ou vazia

---
+++ Entity/Extensions/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrWhiteSpace(this string str)
+        {
+            return String.IsNullOrWhiteSpace(str);
+        }
+    }
+}

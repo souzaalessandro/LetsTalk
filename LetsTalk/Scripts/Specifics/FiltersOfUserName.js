094b84499bf225a7701047ee86386e6743a8db44
samuel borges <samulborges.exe@hotmail.com>
2018-09-06 17:00:38 -0300

    Layout e arquivo do JS para começar a realizar a lógica do filtro..

---
+++ LetsTalk/Scripts/Specifics/FiltersOfUserName.js
@@ -0,0 +1,26 @@
+﻿//copiei do alura só pra ter uma base, as tem  que editar
+//var campoFiltro = document.querySelector("#filtrar-tabela");
+
+//campoFiltro.addEventListener("input", function () {
+//    var pacientes = document.querySelectorAll(".paciente");
+
+//    if (this.value.length > 0) {
+//        for (var i = 0; i < pacientes.length; i++) {
+//            var paciente = pacientes[i];
+//            var tdNome = paciente.querySelector(".info-nome");
+//            var nome = tdNome.textContent;
+//            var expressao = new RegExp(this.value, "i");
+
+//            if (!expressao.test(nome)) {
+//                paciente.classList.add("invisivel");
+//            } else {
+//                paciente.classList.remove("invisivel");
+//            }
+//        }
+//    } else {
+//        for (var i = 0; i < pacientes.length; i++) {
+//            var paciente = pacientes[i];
+//            paciente.classList.remove("invisivel");
+//        }
+//    }
+//});

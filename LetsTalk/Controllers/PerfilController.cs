4a4d997f989dc9bd04fc720a8f0a82bbe60561db
Leo <leo.formaggi@gmail.com>
2018-09-11 14:42:46 -0300

    Mudado o javascript perfil de lugar

--- LetsTalk/Controllers/PerfilController.cs
+++ LetsTalk/Controllers/PerfilController.cs
@@ -47,9 +47,10 @@ namespace MexendoNoTemplate.Controllers
 
         private bool IsImagemValida(HttpPostedFileBase file)
         {
+            byte tamanhoMega = 2;
             string[] formats = new string[] { ".jpg", ".png", ".jpeg" };
             bool validType = file.ContentType.Contains("image");
-            bool validSize = file.ContentLength <= 2 * 1024 * 1024;
+            bool validSize = file.ContentLength <= tamanhoMega * 1024 * 1024;
             bool validExtension = formats.Any(item => file.FileName.EndsWith(item, StringComparison.OrdinalIgnoreCase));
 
             return validType && validSize && validExtension;
@@ -59,12 +60,12 @@ namespace MexendoNoTemplate.Controllers
         public ActionResult SalvarInformacoesPessoais(UsuarioViewModel usuaviewModel)
         {
             Usuario user = new Usuario();
-            List<Tag> tags = new List<Tag>();
+           // List<Tag> tags = new List<Tag>();
 
-            usuaviewModel.Tags.Split(' ').ToList().ForEach(t => tags.Add(new Tag()
-            {
-                Nome = t
-            }));
+            //usuaviewModel.Tags.Split(' ').ToList().ForEach(t => tags.Add(new Tag()
+            //{
+            //    Nome = t
+            //}));
             //UsuarioBLL usuarioBLL = new UsuarioBLL();
             //MvcUser user = (MvcUser)System.Web.HttpContext.Current.User;
             //BLLResponse<Usuario> usuarioAtual = usuarioBLL.LerPorID(usuario.ID);
@@ -77,5 +78,6 @@ namespace MexendoNoTemplate.Controllers
     {
         public string Frase { get; set; }
         public string Tags{ get; set; }
+        public string Descricao{ get; set; }
     }
 }
---
+++ LetsTalk/Scripts/Specifics/Perfil.js
@@ -0,0 +1,22 @@
+﻿function salvar() {
+    var frase = $("#fraseApresentacao").val();
+    var tags = "";
+
+    $("#tags_2_tagsinput").find("span.tag").each(function (i) {
+        var temp = $(this).text();
+        tags += temp.substring(0, temp.length - 1);
+    });
+
+    $.ajax({
+        url: 'SalvarInformacoesPessoais',
+        type: 'POST',
+        data: JSON.stringify({ Frase: frase, Tags: tags }),
+        contentType: "application/json;charset=utf-8",
+        success: function (data) {
+            alert("foi!");
+        },
+        error: function (request, status, error) {
+            alert('oh, errors here. The call to the server is not working.')
+        }
+    });
+}

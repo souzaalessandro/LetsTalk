1bc0a308105682ffc719399e918244d478243c2f
Felipe Bornhofen <felipebornhofen@hotmail.com>
2018-09-14 14:30:21 -0300

    Alteração nos Sliders do filtro e erro de referência resolvido.

--- BusinessLogicalLayer/UsuarioBLL.cs
+++ BusinessLogicalLayer/UsuarioBLL.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-using BusinessLogicalLayer.Interfaces;
 using DataAccessObject;
 using Entity;
 using Entity.Enums;

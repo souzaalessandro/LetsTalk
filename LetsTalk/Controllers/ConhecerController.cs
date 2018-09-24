bc4e77aab734fa50a75dfc768caae0a1c0aabf39
Leo <leo.formaggi@gmail.com>
2018-09-23 23:44:06 -0300

    Retirado referência do DAO do MVC

--- LetsTalk/Controllers/ConhecerController.cs
+++ LetsTalk/Controllers/ConhecerController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using DataAccessObject;
 using Entity;
 using LetsTalk.Models;
 using System.Data.Entity;

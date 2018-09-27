fc900e39efc37d09f486d7d34844732dca3dc2ca
Felipe Bornhofen <felipebornhofen@hotmail.com>
2018-09-27 14:09:33 -0300

    Pequenas alterações em conhecer

---
+++ LetsTalk/Scripts/Specifics/Conhecer.js
@@ -0,0 +1,14 @@
+﻿
+$(window).resize(function () {
+    console.log('resizeou');
+    setFonts();
+})
+
+function setFonts() {
+    var divWidth = $('.card-usuario').width();
+    $('.h3-nome-idade').css('fontSize', divWidth / 13);
+    $('.p-frase').css('fontSize', divWidth / 18);
+    $('.ul-tags').css('fontSize', divWidth / 20);
+    $('.botao-ver-perfil').css('fontSize', divWidth / 20);
+    $('.foto-usuarios').css('width', divWidth).css('height', divWidth);
+}
